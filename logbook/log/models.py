af0fb2bd49dd9c82e0f045934cf01eb9b1fe69cd
Erica Cheong <erica.clw@gmail.com>
2015-08-24 01:56:30 +0800

    feat: complete log CRUD features

--- logbook/log/models.py
+++ logbook/log/models.py
@@ -33,6 +33,15 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True)
 
+    def __repr__(self):
+        return '<Tag %r>' % self.name
+
+    def save(self):
+        """Save the tag to database"""
+        db.session.add(self)
+        db.session.commit()
+        return self
+
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(250))
@@ -44,3 +53,41 @@ class Log(db.Model):
     update_date = db.Column(db.DateTime, default=datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship(User)
+
+
+    def get_tags(self):
+        """Return tag as string seperated by comma"""
+        output = []
+        for t in self.tag:
+            output.append(t.name)
+            print t.name
+        return ','.join(output)
+
+    def set_tags(self, tags):
+        """Set log tags from a list of string tags"""
+        tag_list = tags.split(',')
+        
+        for tag in tag_list:
+            t_obj = Tag.query.filter_by(name=tag).first()
+            if t_obj is None:
+                # add tag to log
+                t = Tag(name=tag.lower())
+                t.save()
+                self.tag.append(t)
+            else:
+                # check if tag is already in log list
+                if not t_obj in self.tag:
+                    self.tag.append(t_obj)
+        return self
+
+    def save(self):
+        """Save log object into database"""
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def delete(self):
+        """Delete log object from database"""
+        db.session.delete(self)
+        db.session.commit()
+        return self
--- logbook/log/views.py
+++ logbook/log/views.py
@@ -9,6 +9,7 @@ from flask import (current_app, Blueprint, render_template, request,
     redirect, url_for, flash)
 from logbook.log.models import User, Tag, Log
 from logbook.extensions import db
+from datetime import datetime
 
 log = Blueprint('log', __name__)
 
@@ -27,30 +28,49 @@ def new_log():
                      user_id=1)  #TODO: get user_id from session
         
         # process tags
-        tags = request.form['tag'].split(',')
+        newlog.set_tags(request.form['tag'])
+        newlog.save()
 
-        for t in tags:
-            print t
-            tag = Tag.query.filter_by(name=t.lower()).first()
-            if tag:
-                newlog.tag.append(tag)
-            else:
-                tag = Tag(name=t.lower())
-                newlog.tag.append(tag)
-        db.session.add(newlog)
-        db.session.commit()
         return redirect(url_for('log.index'))
     else:
         return render_template("log/newlog.html")
 
-@log.route('/<int:log_id>/edit/')
+@log.route('/<int:log_id>/edit/', methods=['GET','POST'])
 def edit_log(log_id):
-    return "This page edits a log entry."
+    log = Log.query.get(log_id)
+    if request.method == 'POST':
+        if request.form['subject']:
+            log.subject = request.form['subject']
+        if request.form['link']:
+            log.link = request.form['link']
+        if request.form['notes']:
+            log.notes = request.form['notes']
+        if request.form['tag']:
+            log.set_tags(request.form['tag'])
+            
+        log.update_date = datetime.now()
+        log.save()
 
-@log.route('/<int:log_id>/delete')
+        flash("Log edited.")
+        return redirect(url_for('log.index'))
+    else:
+        return render_template("log/editlog.html", log=log)
+
+@log.route('/<int:log_id>/delete', methods=['GET','POST'])
 def delete_log(log_id):
+    log = Log.query.get(log_id)
+    if request.method == 'POST':
+        if log:
+            log.delete()
+            flash("Log deleted")
+            return redirect(url_for('log.index'))
+    else:
+        return render_template('log/deletelog.html', log=log)
+
     return "This page deletes a log entry."
 
 @log.route('/tag/<tagname>')
 def show_tag(tagname):
+    logs = Log.query.filter(Log.tag.any(name=tagname)).all()
+    return render_template("index.html", logs=logs)
     return "This page shows all entries with %s." % tagname
